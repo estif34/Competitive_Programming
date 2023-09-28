dc94645beff96de8890ce5795b2845c09aa10480
estif34 <115989802+estif34@users.noreply.github.com>
2023-09-28 13:32:18 +0300

    gradingStudents

---
+++ GradingStudents.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents($grades) {
+    // Write your code here
+    for($i=0; $i<=count($grades); $i++){
+        if($grades[$i]>=38){
+            if((($grades[$i]%5)==3) || (($grades[$i]%5)==4)){
+                $grades[$i] += (5-($grades[$i]%5));
+            }
+        }
+    }
+    return $grades;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$grades_count = intval(trim(fgets(STDIN)));
+
+$grades = array();
+
+for ($i = 0; $i < $grades_count; $i++) {
+    $grades_item = intval(trim(fgets(STDIN)));
+    $grades[] = $grades_item;
+}
+
+$result = gradingStudents($grades);
+
+fwrite($fptr, implode("\n", $result) . "\n");
+
+fclose($fptr);
+
+?>
