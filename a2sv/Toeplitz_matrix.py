cf194987df6428beceeb6896edc5f945de20dd98
estif34 <115989802+estif34@users.noreply.github.com>
2024-04-10 18:05:22 +0300

    longest common prefix

--- a2sv/Toeplitz_matrix.py
+++ a2sv/Toeplitz_matrix.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+    def isToeplitzMatrix(self, matrix: list[list[int]]) -> bool:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if i>0 and j>0 and matrix[i][j] != matrix[i-1][j-1]:
