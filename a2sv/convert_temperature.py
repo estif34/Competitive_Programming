195e9f60d3af1a5c0cb3fcaca58b1df9af842c8a
estif34 <115989802+estif34@users.noreply.github.com>
2024-04-10 18:31:43 +0300

    list comprehension

--- a2sv/convert_temperature.py
+++ a2sv/convert_temperature.py
@@ -1,5 +1,5 @@
 class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
+    def convertTemperature(self, celsius: float) -> list[float]:
         kelvin = celsius + 273.15
         farenheit = celsius * 1.80 + 32.00
         return [kelvin, farenheit]
