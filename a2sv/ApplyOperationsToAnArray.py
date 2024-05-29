c50e93c290d2e6867e871cc64bb53d3cb8855cce
estif34 <115989802+estif34@users.noreply.github.com>
2024-05-29 18:52:39 +0300

    Apply operations to and array

---
+++ a2sv/ApplyOperationsToAnArray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        res = []
+        n = 0
+        for i in range(len(nums)-1):
+            if nums[i] != nums[i+1]:
+                continue
+            else:
+                nums[i] *= 2
+                nums[i+1] = 0
+
+        for j in nums:
+            if j != 0:
+                res.append(j)
+            else:
+                n+=1
+        return res+[0]*n
